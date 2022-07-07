743528ec9df81e342184fa0153e4b082dbee1845
MattFerzz <fnandez.matias@gmail.com>
2022-07-06 23:59:58 -0300

    :sparkles: added highlighting for active choice

--- src/ChoiceSelection.jsx
+++ src/ChoiceSelection.jsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
 const ChoiceSelection = (props) => {
   const options = props.options;
-
+  const [active, setActive] = useState(null);
   const handleClick = (value) => {
     props.handleSelection(value);
+    setActive(value)
   };
   return (
     <div className="container-selection-buttons">
       {options.map((option, index) => {
         return (
           <button
-            className="container-selection-button"
+            className={
+              active === option.value
+                ? "container-selection-button active"
+                : "container-selection-button"
+            }
             key={index}
             value={option.value}
             onClick={() => handleClick(option.value)}
