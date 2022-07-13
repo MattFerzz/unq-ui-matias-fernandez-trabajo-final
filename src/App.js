425cb5c24ec5123459ee7cb32c4e73d2d56eceed
MattFerzz <fnandez.matias@gmail.com>
2022-07-13 01:38:53 -0300

    :children_crossing: add redirect for any route to game

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <BrowserRouter>
         <div>
           <Routes>
-            <Route path="/" element={<Game />} />
+            <Route path="*" element={<Game />} />
           </Routes>
         </div>
       </BrowserRouter>
